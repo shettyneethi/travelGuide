211ba273ceb90a3196a784456ed3c5f161ba37f8
Neethi <neethi.shetty@colorado.edu>
2019-04-13 04:39:52 -0600

    build wordcloud for a location

---
+++ sentiAnalyzer.R
@@ -0,0 +1,74 @@
+# library(readr)
+library(tm)
+library(mongolite)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(SnowballC) # Provides wordStem() for stemming.
+library(RColorBrewer) 
+# library(flipTime)
+# df <- read_csv("reviews.csv")
+# df <- df[which(df$NAME == "Rocky Mountain National Park Tour"),]
+# df <- df[2]
+# print(df)
+df <- mongo(collection = "REVIEWS", db="DATASET")
+# df[which(df$NAME == "Rocky Mountain National Park Tour"),]
+
+df <- df$find(
+  query = '{"NAME" : "Rocky Mountain National Park Tour" }', 
+  fields = '{"REVIEWS" : true}'
+)
+
+docs <- df$REVIEWS[[1]][3][,1]
+# print(docs)
+docs <- Corpus(VectorSource(docs))
+
+
+###########################################################################################
+# Data Cleaning - Applying Transformations
+###########################################################################################
+toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/|@|//|$|:|:)|*|&|!|?|_|-|#|")  ## replace special characters by space
+docs <- tm_map(docs, content_transformer(tolower)) # Conversion to Lower Case
+docs <- tm_map(docs, removePunctuation) # Punctuation can provide gramatical context which supports 
+docs <- tm_map(docs, removeWords, stopwords("english")) # common stop Words like for, very, and, of, are, etc,
+docs <- tm_map(docs, removeWords, c("the", "will", "The", "also", "that", "and", "for", "in", "is", "it", "not", "to"))
+docs <- tm_map(docs, removeNumbers) # removal of numbers
+docs <- tm_map(docs, stripWhitespace) # removal of whitespace
+docs <- tm_map(docs, stemDocument) # Stemming uses an algorithm that removes common word endings for English
+
+dtm <- DocumentTermMatrix(docs)
+tdm <- TermDocumentMatrix(docs)
+
+freq <- sort(colSums(as.matrix(dtm)), decreasing=TRUE)
+wf <- data.frame(word=names(freq), freq=freq)
+
+subset(wf, freq>10)    %>%
+        ggplot(aes(word, freq)) +
+        geom_bar(stat="identity", fill="darkred", colour="darkgreen") +
+        theme(axis.text.x=element_text(angle=45, hjust=1))
+
+
+
+library(wordcloud)
+set.seed(100)
+wordcloud(names(freq), freq, min.freq=10, colors=brewer.pal(6, "Dark2"))
+
+
+# month <- "2009-03"
+# as.Date(paste(month,"-01",sep=""))
+
+
+# review_dates <- df$REVIEWS[[1]]$DATE
+
+# review_dates <- word(review_dates,1)
+
+
+# review_dates_mod <- paste("01",review_dates, sep=" ")
+# review_dates_mod<- as.Date(review_dates,"%d %B %Y")
+
+
+# dim(df$REVIEWS[[1]]); min(review_dates); max(review_dates)
+
+# ggplot(review_dates,aes(DATE, n)) + ggtitle('The Number of Reviews Per Week')
+
